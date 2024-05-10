a6c9a1a0794c527bc8679644de70955f9a4b1959
keremcanseker <kerem.4555@gmail.com>
2024-05-10 20:33:35 +0300

    -some comments and styles -lint, and prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,18 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col justify-start items-center p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-start p-24 ${inter.className}`}
     >
-      <div className="justify-start w-1/2">
-        <h1 className="text-3xl font-bold pb-2">Hello, I'm Emir 👋</h1>
-        <h2 className="text-2xl">I am a software developer based in Türkiye.</h2>
+      <div className="w-1/2 justify-start">
+        <h1 className="pb-2 text-3xl font-bold">Hello, I'm Emir 👋</h1>
+        <h2 className="text-2xl">
+          I am a software developer based in Türkiye.
+        </h2>
       </div>
-      <div className="flex flex-col gap-2 justify-start w-1/2 my-8">
-        <Link href="/posts">See all <span className="underline font-semibold"> Posts</span></Link>
+      <div className="my-8 flex w-1/2 flex-col justify-start gap-2">
+        <Link href="/posts">
+          See all <span className="font-semibold underline"> Posts</span>
+        </Link>
       </div>
     </main>
   );
