493cb23cbd0c7b06ca54309f2a17fdfcf8f95646
Emir Halıcı <emir.halici1@gmail.com>
2024-06-08 02:19:15 +0300

    feat: blog preparation

--- lib/assets/posts.ts
+++ lib/assets/posts.ts
@@ -7,8 +7,9 @@ export interface Post {
 const posts: Post[] = [
   {
     id: 1,
-    title: "Post 1",
-    content: "This is my first post",
+    title: "Sint amet labore culpa fugiat.",
+    content:
+      "Tempor excepteur Lorem ut consectetur do occaecat est exercitation Lorem deserunt voluptate deserunt sunt.Commodo aliquip ipsum elit consectetur sint aliquip. Nulla reprehenderit amet ea commodo velit. Cillum laborum ea Lorem nostrud id. Sint do mollit proident do aute ex pariatur pariatur anim. Ut nisi cillum qui consequat fugiat nostrud dolore reprehenderit esse. Esse exercitation laborum voluptate amet do. Sint sint culpa quis eu sit nostrud incididunt ut excepteur ad dolor tempor occaecat. Velit amet pariatur occaecat adipisicing elit voluptate velit.Commodo consectetur adipisicing commodo dolor cupidatat tempor. Reprehenderit velit incididunt officia consequat fugiat qui cupidatat aliqua sint sint velit. Voluptate sint voluptate labore aute officia deserunt laboris mollit culpa amet ea. Consequat magna proident anim velit dolore sint labore velit pariatur quis voluptate nulla adipisicing. Id dolore pariatur dolore labore ea do Lorem. Pariatur non nulla cillum magna aute qui eiusmod tempor amet incididunt nulla.",
   },
   {
     id: 2,
--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,7 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import { Header } from "~/components/ui/header";
 import posts from "~/lib/assets/posts";
 import { Post } from "~/lib/assets/posts";
+import { Manrope } from "next/font/google";
+import { FaChevronLeft } from "react-icons/fa6";
+import Link from "next/link";
 
+const manrope = Manrope({ subsets: ["latin"] });
 interface PostPageProps {
   post: Post;
 }
@@ -12,10 +17,23 @@ export default function Page({ post }: PostPageProps) {
   }
 
   return (
-    <div className="text-center">
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </div>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-start py-6 px-2 md:px-24 ${manrope.className}`}
+    >
+      <Header />
+      <div className="flex flex-col items-start w-1/2 pt-4">
+        <div className="flex flex-row w-full pb-5 items-center">
+          <Link
+            href="/posts"
+            className="p-2 mr-4 rounded-lg bg-primary-foreground hover:bg-accent border border-input"
+          >
+            <FaChevronLeft />
+          </Link>
+          <h1 className="text-4xl font-semibold">📖 {post.title}</h1>
+        </div>
+        <p>{post.content}</p>
+      </div>
+    </main>
   );
 }
 
