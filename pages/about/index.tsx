7590a780aa22103525cad339976bbef9f20b609c
Emir Halıcı <emir.halici1@gmail.com>
2024-09-10 02:34:37 +0300

    fix: escape char

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <div className="w-11/12 sm:w-1/2 justify-start pt-6">
         <h1 className="pb-5 text-4xl font-semibold">📑 About</h1>
         <p>
-          I'm Emir, a software developer based in Türkiye. I enjoy fiddling with code and building prototypes.
+          I&apos;m Emir, a software developer based in Türkiye. I enjoy fiddling with code and building prototypes.
         </p>
         <h2 className="pt-5 pb-2 text-xl opacity-80 font-semibold">Experience</h2>
         <p>
