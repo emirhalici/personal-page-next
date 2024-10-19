8561ce19244e4197067bd8973e809da2ce211e04
Emir Halıcı <emir.halici1@gmail.com>
2024-10-20 01:05:47 +0300

    chore: formatting

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import { marked } from 'marked';
-import matter from 'gray-matter';
+import fs from "fs";
+import path from "path";
+import { marked } from "marked";
+import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), 'lib/assets/posts');
+const postsDirectory = path.join(process.cwd(), "lib/assets/posts");
 
 export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-  
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
   // Use gray-matter to parse the post metadata section
   const { data, content } = matter(fileContents);
 
@@ -24,22 +24,22 @@ export async function getPostData(id: string) {
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map(fileName => {
+  return fileNames.map((fileName) => {
     return {
       params: {
-        id: fileName.replace(/\.md$/, '')
-      }
+        id: fileName.replace(/\.md$/, ""),
+      },
     };
   });
 }
 
 export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map(fileName => {
-    const id = fileName.replace(/\.md$/, '');
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-    
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
     // Use gray-matter to parse the post metadata section
     const { data } = matter(fileContents);
 
