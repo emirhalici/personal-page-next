0dc201f57bbd3f053acb145606f3f6f011cbcce8
Emir Halıcı <emir.halici1@gmail.com>
2024-10-20 03:45:12 +0300

    feat: 404 not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,20 @@
-// change as you wish
-// if you have a another 404 in another route that will be used instead of this one
+import { Lexend } from "next/font/google";
+import Link from "next/link";
+
+const lexend = Lexend({ subsets: ["latin"] });
+
 export default function Custom404() {
-  return <h1 className="text-center">Not found</h1>;
+  return (
+    <div className={`flex min-h-screen items-center justify-center ${lexend.className}`}>
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">404</h1>
+        <p className="text-2xl font-light">Page not found</p>
+        <Link href="/">
+          <button className="mt-4 border border-input bg-primary-foreground p-2 rounded-lg text-lg font-medium hover:bg-accent">
+            Go back home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
