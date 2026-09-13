d06440693ba273193a943110c937f1ea39ee623e
Emir Halıcı <emir.halici1@gmail.com>
2026-09-13 03:58:58 +0300

    chore: remove Gemstone Lights mention from homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { SocialButtons } from "~/components/ui/cards";
 import { Header } from "~/components/ui/header";
 import { Separator } from "~/components/ui/separator";
-import { Url } from "~/components/ui/url";
 
 
 
@@ -21,9 +20,6 @@ export default function Home() {
         <div className="text-lg opacity-80 font-light space-y-3">
           <p>
             Hi there 👋. I&apos;m Emir, a software developer based in <b>Türkiye</b>. I specialise in mobile applications and it&apos;s supporing infrastructures.
-          </p>
-          <p>
-            I&apos;m currently working at <Url href="https://www.gemstonelights.com/" className={urlStyle}>Gemstone Lights</Url> where I help build the best smart lighting product on the market!
           </p>
           <p>
             I also sometimes write <Link href={"/posts"} className={urlStyle}>blog posts</Link>  here and there about things I find interesting.
